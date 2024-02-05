dd7e47c9e3065bdb4ecb6a24f4979f4b5304f3ce
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-02-05 10:37:45 +0100

    feat: add creation of archive and metadata files

--- upload-cloud-assembly/action.sh
+++ upload-cloud-assembly/action.sh
@@ -4,48 +4,83 @@ IFS=$'\n\t'
 
 parse_args() {
   INPUT_AWS_S3_BUCKET_NAME=""
-  INPUT_CLOUD_ASSEMBLY_ARCHIVE=""
+  INPUT_CDK_APP_DIR=""
   while [ "$#" -gt 0 ]; do
     case "$1" in
-      --aws-s3-bucket-name)     INPUT_AWS_S3_BUCKET_NAME="$2"; shift; shift ;;
-      --cloud-assembly-archive) INPUT_CLOUD_ASSEMBLY_ARCHIVE="$2"; shift; shift ;;
-      *) echo "Unknown option '$1'"; exit 1 ;;
+    --aws-s3-bucket-name)
+      INPUT_AWS_S3_BUCKET_NAME="$2"
+      shift
+      shift
+      ;;
+    --cdk-app-dir)
+      INPUT_CDK_APP_DIR="$2"
+      shift
+      shift
+      ;;
+    *)
+      echo "Unknown option '$1'"
+      exit 1
+      ;;
     esac
   done
   if [ "$INPUT_AWS_S3_BUCKET_NAME" = "" ]; then
-    echo "Parameter 'aws-s3-bucket-name' is empty"; exit 1
+    echo "Parameter 'aws-s3-bucket-name' is empty"
+    exit 1
   fi
-  if [ "$INPUT_CLOUD_ASSEMBLY_ARCHIVE" = "" ]; then
-    echo "Parameter 'cloud-assembly-archive' is empty"; exit 1
+  if [ "$INPUT_CDK_APP_DIR" = "" ]; then
+    echo "Parameter 'cdk-app-dir' is empty"
+    exit 1
   fi
-  readonly INPUT_AWS_S3_BUCKET_NAME CLOUD_ASSEMBLY_ARCHIVE
-  export INPUT_AWS_S3_BUCKET_NAME CLOUD_ASSEMBLY_ARCHIVE
+  readonly INPUT_AWS_S3_BUCKET_NAME INPUT_CDK_APP_DIR
+  export INPUT_AWS_S3_BUCKET_NAME INPUT_CDK_APP_DIR
 }
 
 main() {
+
   parse_args "$@"
-  tmp_folder="$(dirname "$INPUT_CLOUD_ASSEMBLY_ARCHIVE")"
-  cloud_assembly_metadata_file="$tmp_folder/cloud-assembly.json"
-  s3_key="$(sha256sum "$INPUT_CLOUD_ASSEMBLY_ARCHIVE" | cut -d' ' -f1).zip"
-  echo "Uploading local file '$cloud_assembly_metadata_file' to S3 with key '$s3_key'"
-  s3_version_id="$(aws s3api put-object \
-    --bucket "$INPUT_AWS_S3_BUCKET_NAME" \
-    --key "$s3_key" \
-    --body "$INPUT_CLOUD_ASSEMBLY_ARCHIVE" \
-    --query "VersionId" \
-    --output text
+
+  if [ ! -f "${INPUT_CDK_APP_DIR}/cdk.json" ]; then
+    echo "No cdk.json file found in '$INPUT_CDK_APP_DIR'. Please set the 'cdk-app-dir' input" \
+      " to a directory containing a CDK App." >&2
+    exit 1
+  fi
+
+  tmp_dir="/tmp/${GITHUB_ACTION:-$(date -u +%s)}"
+  mkdir -p "$tmp_dir"
+
+  target_archive_file="$tmp_dir/cloud-assembly.zip"
+  target_metadata_file="$tmp_dir/cloud-assembly.json"
+
+  echo "Creating Cloud Assembly archive at '$target_archive_file' from CDK application in '$INPUT_CDK_APP_DIR'"
+  cd "$INPUT_CDK_APP_DIR"
+  rm -rf cdk.out
+  npx cdk synth
+  cd cdk.out
+  zip -r "$target_archive_file" .
+  s3_key="$(sha256sum "$target_archive_file" | cut -d' ' -f1).zip"
+
+  echo "Uploading Cloud Assembly archive file at '$target_archive_file' to S3 with key '$s3_key'"
+  s3_version_id="$(
+    aws s3api put-object \
+      --bucket "$INPUT_AWS_S3_BUCKET_NAME" \
+      --key "$s3_key" \
+      --body "$target_archive_file" \
+      --query "VersionId" \
+      --output text
   )"
-  cat <<EOF > "$cloud_assembly_metadata_file"
+
+  cat <<EOF >"$target_metadata_file"
 {
   "cloudAssemblyBucketName": "$INPUT_AWS_S3_BUCKET_NAME",
   "cloudAssemblyBucketKey": "$s3_key",
   "cloudAssemblyVersionId": "$s3_version_id"
 }
 EOF
-  echo "Storing Cloud Assembly metadata file at '$cloud_assembly_metadata_file' with contents:"
-  cat "$cloud_assembly_metadata_file"
+
+  echo "Storing Cloud Assembly metadata file at '$target_metadata_file' with contents:"
+  cat "$target_metadata_file"
   if [ "${GITHUB_ACTIONS:-false}" = "true" ]; then
-    echo "cloud-assembly-metadata-file=$cloud_assembly_metadata_file" >> "$GITHUB_OUTPUT"
+    echo "cloud-assembly-metadata-file=$target_metadata_file" >>"$GITHUB_OUTPUT"
   fi
 }
 

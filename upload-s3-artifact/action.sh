e92a4001e9a974bcaf12d51c6abf61196a47b327
evan-boone <ebo@capraconsulting.no>
2024-02-19 12:56:32 +0100

    Add optional prefix to upload-s3-artifact

--- upload-s3-artifact/action.sh
+++ upload-s3-artifact/action.sh
@@ -5,11 +5,13 @@ IFS=$'\n\t'
 parse_args() {
   INPUT_AWS_S3_BUCKET_NAME=""
   INPUT_AWS_S3_KEY=""
+  INPUT_AWS_S3_KEY_PREFIX=""
   INPUT_TARGET_PATH=""
   while [ "$#" -gt 0 ]; do
     case "$1" in
       --aws-s3-bucket-name)     INPUT_AWS_S3_BUCKET_NAME="$2"; shift; shift ;;
       --aws-s3-key) INPUT_AWS_S3_KEY="$2"; shift; shift ;;
+      --aws-s3-key-prefix) INPUT_AWS_S3_KEY_PREFIX="$2"; shift; shift ;;
       --target-path) INPUT_TARGET_PATH="$2"; shift; shift ;;
       *) echo "Unknown option '$1'"; exit 1 ;;
     esac
@@ -20,8 +22,8 @@ parse_args() {
   if [ "$INPUT_TARGET_PATH" = "" ]; then
     echo "Parameter 'target-path' is empty"; exit 1
   fi
-  readonly INPUT_AWS_S3_BUCKET_NAME INPUT_AWS_S3_KEY INPUT_TARGET_PATH
-  export INPUT_AWS_S3_BUCKET_NAME INPUT_AWS_S3_KEY INPUT_TARGET_PATH
+  readonly INPUT_AWS_S3_BUCKET_NAME INPUT_AWS_S3_KEY INPUT_AWS_S3_KEY_PREFIX INPUT_TARGET_PATH
+  export INPUT_AWS_S3_BUCKET_NAME INPUT_AWS_S3_KEY INPUT_AWS_S3_KEY_PREFIX INPUT_TARGET_PATH
 }
 
 main() {
@@ -40,13 +42,14 @@ main() {
     echo "No file or directory at path '$INPUT_TARGET_PATH'"
     exit 1
   fi
+  aws_s3_key_prefix="${INPUT_AWS_S3_KEY_PREFIX:-""}"
   checksum="$(sha256sum "$file_path" | cut -d' ' -f1)"
   filename="$(basename "$file_path")"
   # NOTE: Get the last extension, if any
   extension="$(echo "$filename" | sed -n 's/^.*\.\(.*\)$/\1/p')"
   default_aws_s3_key="$checksum${extension:+.}${extension:-}"
   aws_s3_key="${INPUT_AWS_S3_KEY:-$default_aws_s3_key}"
-  aws s3 cp "$file_path" "s3://$INPUT_AWS_S3_BUCKET_NAME/$aws_s3_key"
+  aws s3 cp "$file_path" "s3://$INPUT_AWS_S3_BUCKET_NAME/${aws_s3_key_prefix}${aws_s3_key}"
   if [ "${GITHUB_ACTIONS:-false}" = "true" ]; then
     echo "aws-s3-key=$aws_s3_key" >> "$GITHUB_OUTPUT"
   fi
